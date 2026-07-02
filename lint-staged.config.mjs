2df9d3b05430fb3e4412859e9509f491051a9faf
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-07-02 23:53:29 +0300

    feat: improve js support for lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,11 @@
 export default {
-  '*.{js,jsx,ts,tsx}': ['./node_modules/.bin/eslint --fix', './node_modules/.bin/prettier --write'],
-  '*.{css,scss}': ['./node_modules/.bin/stylelint --fix', './node_modules/.bin/prettier --write'],
+  '*.{js,jsx,ts,tsx,mjs,cjs}': [
+    './node_modules/.bin/eslint --fix --cache',
+    './node_modules/.bin/prettier --write',
+  ],
+  '*.{css,scss}': [
+    './node_modules/.bin/stylelint --fix --cache',
+    './node_modules/.bin/prettier --write',
+  ],
   '*.{json,md,yml,yaml}': ['./node_modules/.bin/prettier --write'],
 };
