a72fa52f32e47b92a8e773609ed944793d42c1c4
Anna Demyanovich <makarenko-an@part-kom.ru>
2026-07-12 15:25:06 +0300

    feat: simple Footer design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link';
+import styles from './Footer.module.css';
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <span>© {currentYear} SwaggerUI</span>
+      <Link href="/about">О проекте</Link>
+    </footer>
+  );
 };
